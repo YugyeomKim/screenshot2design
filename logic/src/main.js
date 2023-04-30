70f0a624aae86c75c6f6845b1247376fa29279b8
ygkim <hoodie0512@gmail.com>
2023-04-30 19:01:04 +0900

    reset for https (using aws)

--- logic/src/main.js
+++ logic/src/main.js
@@ -1,8 +1,5 @@
 // @ts-check
 
-require('dotenv').config()
-const https = require('https')
-const fs = require('fs')
 const express = require('express')
 const cors = require('cors')
 const createError = require('http-errors')
@@ -38,25 +35,11 @@ app.use((err, req, res, next) => {
 
 app.use('/auth', authRouter)
 app.use('/run', runRouter)
+app.use('/health-check', (req, res) => {
+  res.status(200).send()
+})
 
-if (process.env.NODE_ENV === 'production') {
-  const { SSL_CERT_PATH, SSL_KEY_PATH } = process.env
-  if (!SSL_CERT_PATH || !SSL_KEY_PATH) {
-    throw new Error('No ssl certificate loaded.')
-  }
-
-  const sslOptions = {
-    cert: fs.readFileSync(SSL_CERT_PATH),
-    key: fs.readFileSync(SSL_KEY_PATH),
-  }
-
-  https.createServer(sslOptions, app).listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`listening on ${PORT}`)
-  })
-} else {
-  app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`listening on ${PORT}`)
-  })
-}
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`listening on ${PORT}`)
+})
