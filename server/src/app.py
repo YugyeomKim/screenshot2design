2a04d959a4991eb6d34ea62dc492bfcfa1c2a307
YugyeomKim <hoodie0512@gmail.com>
2024-03-07 22:58:41 +0900

    Fix: Allow all domains of CORS policy

--- server/src/app.py
+++ server/src/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from routers import auth, run_model, handle_data
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins="*")
 app.register_blueprint(run_model.run_bp)
 app.register_blueprint(auth.auth_bp)
 app.register_blueprint(handle_data.handle_data_bp)
